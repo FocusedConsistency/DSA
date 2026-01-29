db79f200fef4659d70477910f28dd4a6532b1766
Focus <Nature.Balance@proton.me>
2026-01-29 02:44:47 +0200

    Create container_with_most_water.py

---
+++ Two_Pointers/container_with_most_water.py
@@ -0,0 +1,19 @@
+def max_area(self, heights: List[int]):
+    left = 0
+    right = len(heights) - 1
+    area_max = 0
+
+    while left < right:
+        # Calculating current area and storing it if it's larger than previous max
+        width = right - left
+        height = min(heights[left], heights[right])
+        area = width * height
+        area_max = max(area_max, area)
+
+        # Move the shorter pointer towards the other
+        if heights[left] > heights[right]:
+            right -= 1
+        else:
+            left += 1
+
+    return area_max
